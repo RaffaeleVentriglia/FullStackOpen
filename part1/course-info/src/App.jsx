42f4ee38f2a62b6672738e5ce620c19683bf09a2
svilu <svilu@TEAMSVILUPPO>
2025-12-29 01:38:05 +0100

    course-info part1 completed

---
+++ part1/course-info/src/App.jsx
@@ -0,0 +1,80 @@
+const Test = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> // Total exercises
+    </div>
+  )
+}
+
+
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part_exercises[0].part1} exercises={props.part_exercises[0].exercises1} />
+      <Part part={props.part_exercises[1].part2} exercises={props.part_exercises[1].exercises2} />
+      <Part part={props.part_exercises[2].part3} exercises={props.part_exercises[2].exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part_exercises = [
+    {part1: 'Fundamentals of react', exercises1: 10},
+    {part2: 'Using props to pass data', exercises2: 7},
+    {part3: 'State of a component', exercises3: 14}
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part_exercises={part_exercises} />
+      <Total exercises1={part_exercises[0].exercises1} exercises2={part_exercises[1].exercises2} exercises3={part_exercises[2].exercises3} />
+    </div>
+  )
+}
+
+export default App
